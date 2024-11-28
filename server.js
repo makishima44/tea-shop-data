17c5fa69d9c6db83eeed2dd8d3c7355b43b27afc
Artur <metallist15431543@gmail.com>
2024-11-28 20:56:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const jsonServer = require('json-server');
-const path = require('path');
-
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json')); // Путь к db.json
+const router = jsonServer.router('db.json'); // Подключение базы
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(3000, () => {
-  console.log('JSON Server is running');
+// Слушаем порт, предоставляемый Vercel
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
+
