d1de9b3ba9bada2fe72bae5c12b15bed3db091d9
Artur <metallist15431543@gmail.com>
2024-11-28 21:07:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Подключение базы
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
 
-// Слушаем порт, предоставляемый Vercel
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+// Включение CORS
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
 });
 
+server.use(router);
+
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
